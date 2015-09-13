2ff8f51ef9076d97b7633fb126855e918fb4181c
jrior001 <jriordan001@gmail.com>
2015-09-13 09:05:03 -0400

    Move KERNEL_SOURCE define out of msm8974-common
    Change-Id: I500c2118cddb9a83acf9dbb8655833dc0aef445a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,6 +20,7 @@
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 TARGET_KERNEL_CONFIG := cyanogenmod_find7_defconfig
+TARGET_KERNEL_SOURCE := kernel/oppo/msm8974
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oppo/find7-common/bluetooth
