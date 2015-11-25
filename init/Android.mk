CyanogenMod/android_device_oneplus_bacon

init: Set read-only properties with the radio and PCB versions
Change-Id: I2f480fd7642a749dfd2452f60283b68a55a5b81f

init/Android.mk
@@ -1,0 +1,10 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall 
+LOCAL_SRC_FILES := init_model.c
+LOCAL_MODULE := libinit_bacon
+include $(BUILD_STATIC_LIBRARY)

CyanogenMod/android_device_oppo_find7-common

init/Android.mk
@@ -1,0 +1,11 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_C_INCLUDES := system/core/init
+LOCAL_CFLAGS := -Wall
+LOCAL_SRC_FILES := init_find7.c
+LOCAL_MODULE := libinit_find7
+
+include $(BUILD_STATIC_LIBRARY)